9f378f6907c2b0575a613697703afa1617f6b6c2
beardykraken <vcasmirri@gmail.com>
2020-02-09 21:12:11 -0500

    Both players are able to enter their names. Their info is populated in separate cards. Next step will be to build game logic.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,13 +15,17 @@ var firebaseConfig = {
 var database = firebase.database();
 
 // Database variables
-var currentTurn = database.ref();
+var currentStep = database.ref();
 var connectionsRef = database.ref("/connections");
 var playerOne = database.ref("/connections/playerOne");
 var playerTwo = database.ref("/connections/playerTwo");
 
 // Global variables
 var player;
+var player1;
+var player2;
+// var playerOne = null;
+// var playerTwo = null;
 var playerOneSnap;
 var playerTwoSnap;
 var playerOneChoice;
@@ -30,6 +34,7 @@ var playerOneWins = 0;
 var playerTwoWins = 0;
 var playerOneLosses = 0;
 var playerTwoLosses = 0;
+var playerOrder = 0;
 
 
 // var connectedRef = database.ref(".info/connected");
@@ -55,15 +60,84 @@ $("#submitName").on("click", function() {
     event.preventDefault();
     player = $("#name").val().trim();
     playerOne.once("value", function (snapshot) {
-        playerOneSnap
-    })
+        playerOneSnap = snapshot;
+    }, function (errorObject) {
+        console.log("Failed to read. Code: " + errorObject.code);
+    });
+    playerTwo.once("value", function (snapshot) {
+        playerTwoSnap = snapshot;
+    }, function (errorObject) {
+        console.log("Failed to read. Code: " + errorObject.code);
+    });
+
+    if (!playerOneSnap.exists()) {
+        playerOrder = 1;
+        playerOne.onDisconnect().remove();
+        playerOne.set({
+            player: player,
+            winCount: 0,
+            lossCount: 0
+        });
+        $(".messages").empty();
+        var messages = $("<div>");
+        messages.addClass("col-12 p-4 text-center");
+        messages.html("<h3>Welcome, " + player + "! You're the first to arrive. Waiting on Player 2.</h3>");
+        $(".messages").append(messages);
+    } else if (playerOneSnap) {
+        playerOrder = 2;
+        playerTwo.onDisconnect().remove();
+        playerTwo.set({
+            player: player,
+            winCount: 0,
+            lossCount: 0
+        })
+        $(".messages").empty();
+        var messages = $("<div>");
+        messages.addClass("col-12 p-4 text-center");
+        messages.html("<h3>Welcome, " + player + "! Let's begin. Waiting for " + player1 + " to choose.</h3>");
+        $(".messages").append(messages);
+        currentStep.update({
+            step: 1
+        })
+    } else [
+
+    ]
+    console.log("Player order is " + playerOrder);
 })
 
 
 // Update info for player one when database updates
 playerOne.on("value", function (snapshot) {
     if (snapshot.val() !== null) {
+        player1 = snapshot.val().player;
+        $(".firstPlayerBox").empty();
+        var playerOneTitle = $("<h4>");
+        playerOneTitle.addClass("card-header");
+        playerOneTitle.text(player1);
+        $(".firstPlayerBox").append(playerOneTitle);
+    } else {
+        $(".firstPlayerBox").html("Player 1 has not yet entered");
+    }},
+    function (errorObject) {
+        console.log("Failed to read. Code: " + errorObject.code);
+    });
 
-    }
+// Update info for player one when database updates
+playerTwo.on("value", function (snapshot) {
+    if (snapshot.val() !== null) {
+        player2 = snapshot.val().player;
+        $(".secondPlayerBox").empty();
+        var playerTwoTitle = $("<h4>");
+        playerTwoTitle.addClass("card-header");
+        playerTwoTitle.text(player2);
+        $(".secondPlayerBox").append(playerTwoTitle);
+    } else {
+        $(".secondPlayerBox").html("Player 2 has not yet entered");
+    }},
+    function (errorObject) {
+        console.log("Failed to read. Code: " + errorObject.code);
+    });
+
+currentStep.on("value", function (snapshot) {
+    console.log("The current step is: " + snapshot.val().step);
 })
-    
